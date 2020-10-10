2f236a23c2bde67c75bf111d6d8227f6b823c6ac
Almir Campos <almircampos@gmail.com>
2020-10-10 03:28:16 -0700

    [U14] 14.3 - Exercise 1 - Part 3 - Exercise completed

--- 14.3/exercises/ajax-giphy-party/app.js
+++ 14.3/exercises/ajax-giphy-party/app.js
@@ -4,17 +4,32 @@ console.log("Let's get this party started!");
 
 const form = $('#search_form');
 const expr = $('#search_expr');
+const gifs = document.getElementById('gifs');
 const imgs = document.getElementById('imgs');
 const ltnr = form.on('submit', (e) => {
 	e.preventDefault();
+	e.stopPropagation();
 	const val = expr.val();
 	if ( !val) {
 		console.log('Nothing to search...');
 		return;
 	}
+	// console.log( e. );
+	if (e.originalEvent.submitter.id === 'removeAll') {
+		removeAll();
+		return;
+	}
 	processSearch(val)
-		.then(r => r.data.data.filter((r, index) => index < 3)
-								.forEach(r => insertGif( r.images.original.url) ));
+		.then(r => {
+			const data = r.data.data;
+			const l    = data.length;
+			const arr  = [ rInt(l), rInt(l), rInt(l)];
+			console.log(data, l, arr );
+			data
+				.filter((r, index) => arr.includes( index ) )
+				// .forEach(r => console.log(r));
+				.forEach(r => insertGif(r.images.original.url));
+		});
 });
 
 async function processSearch(val) {
@@ -26,10 +41,19 @@ async function processSearch(val) {
 	return result;
 }
 
-function insertGif( url ){
-	const img = buildImg( url );
-	imgs.insertAdjacentHTML('afterend', img );
+function insertGif(url) {
+	const img = buildImg(url);
+	imgs.insertAdjacentHTML('afterend', img);
 }
-function buildImg( url ){
-	return `<img src=${url} height=200 alt=''/>`;
+
+function buildImg(url) {
+	return `<img src="${url}" height="200" alt=""/>`;
+}
+
+function removeAll() {
+	gifs.querySelectorAll('img').forEach(img => img.remove());
+}
+
+function rInt(max) {
+	return (Math.random() * max | 0);
 }
