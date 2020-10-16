2e68a23e38c59d38fe87041e0d9a0976b2063869
Almir Campos <almircampos@gmail.com>
2020-10-15 17:43:34 -0700

    [U14] 14.3-Exercise 2 - TVMaze - Part 4 - Cleaned up and documented

--- 14.3/exercises/apis-tvmaze/tvmaze.js
+++ 14.3/exercises/apis-tvmaze/tvmaze.js
@@ -17,87 +17,89 @@
       }
  */
 
+const FALLBACK_IMAGE_URL = 'https://tinyurl.com/tv-missing';
+
+/**
+ * This is an experiment with 'axios' to learn how to use its 'create' method.
+ */
 const search = axios.create({
 	baseURL: 'http://api.tvmaze.com/search',
 	timeout: 5000
 });
 
-// const images = axios.create({
-// 	url: 'http://api.tvmaze.com/shows',
-// 	timeout: 2000
-// });
-
+/**
+ * This function is responsible for searching the shows and return a structured list of shows that match
+ * the search criteria (query)
+ * @param query	String to be searched
+ * @returns {Promise<[]>}	Promise that (in this project) is fulfilled in the function 'handleSearch'
+ */
 async function searchShows(query) {
-	// TODO: Make an ajax request to the searchShows api.  Remove
-	// hard coded data.
 
+	/**
+	 * Initial variables
+	 */
 	const searchRes = await (search.get('/shows', {params: {q: query}}));
-	console.log('searchRes:', searchRes);
-	const resData = searchRes.data;
-	// console.log('resData/before:', resData);
-	// resData.forEach(res => console.log(res.show.image.medium));
-	// console.log('resData/after:', resData);
-	let shows = [];
-	let imgUrl;
-	let img;
-	// async function checkImage (shows ) {
-	// 	const shois = shows.clone();
-	// 	shows.forEach( show => {
-	//
-	//
-	//
-	// 	});
-	// };
+	const resData   = searchRes.data;
+	let shows       = [];
+	let image;
 
-	const checkOneUrl = async function (imgUrl) {
-		return await axios
-			.get(imgUrl)
-			.then(() => true)
-			.catch(() => false);
-	};
-
-	// console.log( 'before')
-	// console.log( 'checkOneImage', await
-	// checkOneImage('http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpgx') ); console.log( 'after'
-	// );
-
-	const updateShows = async function (meta) {
-		console.log('updateShows 1/meta:', meta);
-		imgUrl = meta.show.image;
-		if ( !imgUrl || !imgUrl.medium) {
-			imgUrl = 'https://tinyurl.com/tv-missing';
-		} else {
-			await checkOneUrl(imgUrl.medium).then(isValidUrl => {
-				console.log('isValidUrl', isValidUrl);
-				if ( !isValidUrl) {
-					imgUrl = 'https://tinyurl.com/tv-missing';
-				} else {
-					imgUrl = imgUrl.medium;
-				}
-			});
-		}
-		console.log('imgUrl:', imgUrl);
-		console.log('updateShows 2', shows);
-		shows.push({id: meta.show.id, name: meta.show.name, summary: meta.show.summary, imgUrl});
-	}
-
-	const foritch = async function (resD) {
-		for (let meta in resD) {
-			await updateShows(resData[meta]);
+	/**
+	 * Generates a structured show list form the raw list obtained directly for the api
+	 * NOTE:	this method checks not only if the image url exists but also if it's valid, in case it exists.
+	 * @param rawShowList
+	 * @returns {Promise<[]>}
+	 */
+	const makeStructuredList = async function (rawShowList) {
+		for (let rawShowItem in rawShowList) {
+			await updateShowsImages(resData[rawShowItem]);
 		}
 		return shows;
 	};
-	console.log('before the foreach');
-	return foritch(resData);
-	// shows = await checkImage ( shows );
-	// console.log('shows:', shows);
-	// return shows;
+
+	const updateShowsImages = async function (meta) {
+
+		/**
+		 * Checks if a url is valid (it exists and is available).
+		 * Note:	this method is generic, but (in this project) is used only in its parent function (this).
+		 * 				That's why it's nested here.
+		 * @param imageUrl
+		 * @returns {Promise<T|boolean>}	Ultimately, return true (the url exists and is available) or false.
+		 */
+		const checkOneUrl = async function (imageUrl) {
+			return await axios
+				.get(imageUrl)
+				.then(() => true)
+				.catch(() => false);
+		};
+
+		/**
+		 * Generates the right image (url): the actual one or the fallback.
+		 * @type {string|string}
+		 */
+		image = meta.show.image;
+		if ( !image || !image.medium) {
+			image = FALLBACK_IMAGE_URL;
+		} else {
+			await checkOneUrl(image.medium).then(isValidUrl => {
+				if ( !isValidUrl) {
+					image = FALLBACK_IMAGE_URL;
+				} else {
+					image = image.medium;
+				}
+			});
+		}
+		/**
+		 * Updates the structured show list with the right image utl
+		 */
+		shows.push({id: meta.show.id, name: meta.show.name, summary: meta.show.summary, image});
+	};
+
+	return makeStructuredList(resData);
 }
 
 /** Populate shows list:
  *     - given list of shows, add shows to DOM
  */
-
 function populateShows(shows) {
 	const $showsList = $("#shows-list");
 	$showsList.empty();
@@ -106,7 +108,7 @@ function populateShows(shows) {
 		let $item = $(
 			`<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
-         <img class="card-img-top" src="${show.imgUrl}">
+         <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
@@ -123,10 +125,13 @@ function populateShows(shows) {
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
-
 $("#search-form").on("submit", async function handleSearch(evt) {
 	evt.preventDefault();
 
+	/**
+	 * If the there's not data to be searched for, just return.
+	 * @type {jQuery}
+	 */
 	let query = $("#search-query").val();
 	if ( !query) {
 		return;
@@ -134,8 +139,16 @@ $("#search-form").on("submit", async function handleSearch(evt) {
 
 	$("#episodes-area").hide();
 
+	/**
+	 * Generates a structured show list as required by the function 'populateShows',
+	 * by searching for the query provided by the user.
+	 * @type {*[]}	Search expression (query) provided by ths user
+	 */
 	let shows = await searchShows(query);
 
+	/**
+	 * Updated the DOM based on the list provided by the the 'searchShows' function.
+	 */
 	populateShows(shows);
 });
 
